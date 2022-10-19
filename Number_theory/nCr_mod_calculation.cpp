f9a5dc7b3697f0d7a9f3d985a4e970e844eb9a53
Ayush_shaz <ayushsharmanitt@gmail.com>
2022-10-19 13:54:22 +0530

    Template added

--- Number_theory/nCr_mod_calculation.cpp
+++ Number_theory/nCr_mod_calculation.cpp
@@ -1,6 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+const ll mod = 1000000007LL;
+
+int power(int a, int b) {
+  int result = 1;
+  while (b) {
+    if (b & 1)
+      result = (result * a) % mod;
+    a = (a * a) % mod;
+    b /= 2;
+  }
+  return result;
+}
+
+int fact[1000005];
+int inv[1000005];
+
+void precompute() {
+  fact[0] = 1;
+  fact[1] = 1;
+  for (int i = 2; i <= 1000004; i++)
+    fact[i] = (fact[i - 1] * i) % mod;
+  inv[1000004] = power(fact[1000004], mod - 2);
+  for (int i = 1000003; i >= 0; i--)
+    inv[i] = ((i + 1) * inv[i + 1]) % mod;
+}
+
+int ncr(int n, int r) {
+  if (r > n || r < 0 || n < 0)
+    return 0;
+  int x = (fact[n] * inv[r]) % mod;
+  return (x * inv[n - r]) % mod;
+}
+
+
+
+
 int x,y,gcd_value;    //ax+by=gcd
 void extended_euclid(int a,int b){ // Extended
     if(b==0){   // reached last
